99d5b19143aa9d6ce234fdbec17a162fb092d4eb
cuizhenCZ <cuizhen97@gmail.com>
2017-08-14 19:49:10 +0800

    Update addWeighted.cpp
    数据的细化整理

--- addWeighted.cpp
+++ addWeighted.cpp
@@ -9,32 +9,50 @@ int addWeight(Rect boundRect,int &hei,int &wid)
 {
 
 
+  if(boundRect.width<70||boundRect.height<90)
+    {
+        if(boundRect.width>=40)
+        {
+            wid = 1;
+        }
+        else
+        {
+            wid = 0;
+        }
+        if(boundRect.height>=60)
+        {
+            hei = 1;
+        }
+        else
+        {
+            hei = 0;
+        }
+        return hei*wid;
+    }
+
+
     switch(boundRect.y/100)
     {
     case 0:
-        wid = (boundRect.width)/36;
-        hei = (boundRect.height)/60;
+        wid = (boundRect.width-70)/30+1;
+        hei = (boundRect.height-90)/38+1;
         break;
     case 1:
-        wid = (boundRect.width)/42;
-        hei = (boundRect.height)/70;
+        wid = (boundRect.width-80)/30+1;
+        hei = (boundRect.height-105)/38+1;
         break;
     case 2:
-        wid = (boundRect.width)/48;
-        hei = (boundRect.height)/80;
+        wid = (boundRect.width-80)/30+1;
+        hei = (boundRect.height-115)/38+1;
         break;
-    case 3:
-        wid = (boundRect.width)/54;
-        hei = (boundRect.height)/90;
-        break;
+
     default:
-        wid = (boundRect.width)/60;
-        hei = (boundRect.height)/100;
+        wid = (boundRect.width-85)/30+1;
+        hei = (boundRect.height-95)/38+1;
         break;
 
 
     }
-
 
  /*   cout<<"----------------------------------------"<<endl;
     cout<<"1.This is addWeight"<<endl;
