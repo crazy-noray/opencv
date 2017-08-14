0f4343252d003101969f80dfaceb62be3349ba7a
cuizhenCZ <cuizhen97@gmail.com>
2017-08-14 19:52:43 +0800

    Update draw.cpp

--- draw.cpp
+++ draw.cpp
@@ -4,56 +4,59 @@
 void draw(Mat frame ,Mat back,int &people)
 {
 
-
     Mat show = frame.clone();
 
+    //contours角点的容器，hierarchy 层容器
     vector<vector<Point> > contours;
     vector<Vec4i>hierarchy;
 
     findContours(back,contours,hierarchy,
                  RETR_TREE,CHAIN_APPROX_SIMPLE,Point(0,0));
-    int area[100000];
+
+    //轮廓面积，外部轮廓，外接矩形
+    int area[10000];
     vector< vector<Point> >contours_poly(contours.size());
     vector<Rect>boundRect(contours.size());
 
     for(unsigned int i = 0;i<contours.size();i++)
     {
-        int hei = 0;
-        int wid = 0;
-        int sum = 0;
-        int count =0;
-        area[i] = contourArea(contours[i]);
+        int hei = 0;//高度权重
+        int wid = 0;//宽度权重
+        int count =0;//框内估计人数
+
+        area[i] = contourArea(contours[i]);//求轮廓面积
 
         if(area[i]>1200)
         {
             approxPolyDP(Mat(contours[i]),contours_poly[i],3,true);
             boundRect[i] = boundingRect(Mat(contours_poly[i]));
 
-            sum = addWeight(boundRect[i],hei,wid);
+            count = addWeight(boundRect[i],hei,wid);
 
-            if(sum == 0)
-            {
-               continue;
-            }
-            else
-            {
-              Count(count,sum,wid);
-            }
-
-          /*  cout<<"------------------------------------------"<<endl;
+            peopleDeal(boundRect[i],count);
+/*
+            cout<<"------------------------------------------"<<endl;
             cout<<"3.draw.."<<endl;
             cout<<"The height is "<<boundRect[i].height<<endl;
             cout<<"The width is "<<boundRect[i].width<<endl;
-            cout<<"sum is "<<sum<<endl;
             cout<<"the count is"<<count<<endl;
             cout<<"------------------------------------------"<<endl;
 */
-            Scalar green= Scalar(0,255,0);
-            rectangle(show,boundRect[i].tl(),boundRect[i].br(),green,2,8,0);
+            drawRect(show,boundRect[i],count);
 
-            people+=count;
+            if(boundRect[i].height>128&&boundRect[i].width>64)
+            {
+                Mat mask = show(Range(boundRect[i].y,boundRect[i].y+boundRect[i].height),
+                                 Range(boundRect[i].x,boundRect[i].x+boundRect[i].width));
+                people += dete(mask);
+            }
+            else
+            {
+                 people+=count;//获得当前视频 范围内人数
+            }
 
-            text(show,count,boundRect[i].x+5,boundRect[i].y+3,1);
+
+
 
         }//面积判断结束
     }//区域循环结束
@@ -63,4 +66,19 @@ void draw(Mat frame ,Mat back,int &people)
 
     imshow("draw",show);
 
+
+}
+void drawRect(Mat show,Rect boundRect,int count)
+{
+    if(count != 0 )
+    {
+        boundRect.y -= 5;
+        boundRect.height -= 10;
+
+        Scalar green= Scalar(0,255,0);
+        rectangle(show,boundRect.tl(),boundRect.br(),green,2,8,0);
+
+        text(show,count,boundRect.x+5,boundRect.y+3,1);//显示到屏幕上
+
+    }
 }
